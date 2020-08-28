8b2d68d33dc9d2374995123666415ef6dfe56586
José Aja <jose.aja@outlook.com>
2020-08-28 12:29:38 +0200

    finished day 3

--- Days/Day3.cs
+++ Days/Day3.cs
@@ -10,6 +10,13 @@ namespace AdventOfCode2019.Days
 {
     class Day3
     {
+        public static Dictionary<string, int[]> DIRECTIONS = new Dictionary<string, int[]>() {
+                                                                                        {"U", new int[2] {0, 1}},
+                                                                                        {"D", new int[2] {0, -1}},
+                                                                                        {"R", new int[2] {1, 0}},
+                                                                                        {"L", new int[2] {-1, 0}}
+                                                                                      };
+
         public Day3()
         { }
 
@@ -20,8 +27,11 @@ namespace AdventOfCode2019.Days
             Console.WriteLine("----------------------------");
 
             string[][] wires = LoadWires();
-            if (wires != null)
-                CalculateCrossings(wires);
+            if (wires != null && wires.Length == 2)
+            {
+                int distance = CalculateMinManhattanDistance(wires);
+                PrintManhattanDistance(distance);
+            }
             else
                 Console.WriteLine("Wrong input");
         }
@@ -29,85 +39,82 @@ namespace AdventOfCode2019.Days
         private string[][] LoadWires()
         {
             string[][] wires = null;
-            string[] lines = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\Files\\Day1-Input.txt");
+            string[] lines = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\Files\\Day3-Input.txt");
             if (lines.Length == 2)
-            {
                 wires = new string[][] { lines[0].Split(','), lines[1].Split(',') };
-            }
+
             return wires;
         }
 
-        private ArrayList CalculateCrossings(string[][] wires)
+        // https://math.stackexchange.com/questions/139600/how-do-i-calculate-euclidean-and-manhattan-distance-by-hand
+        private int CalculateMinManhattanDistance(string[][] wires)
         {
-            ArrayList crossings = new ArrayList();
+            int minDistance = -1;
 
-            int[] initialPointWire1 = new int[2]{ 0, 0}; 
-            for (int i = 0; i < wires[0].Length; i++)
+            List<int[]> pointsWire1 = TransformToArrayOfPoints(wires[0]);
+            List<int[]> pointsWire2 = TransformToArrayOfPoints(wires[1]);
+            List<int[]> crossings = GetCrossings(pointsWire1, pointsWire2);
+            foreach (int[] point in crossings)
             {
-                int[] vector1 = TransformToVector(wires[0][i]);
+                int actualDistance = Math.Abs(point[0]) + Math.Abs(point[1]); 
+                if (actualDistance < minDistance || (minDistance < 0))
+                    minDistance = actualDistance;
 
-                int[] finalPointWire1 = new int[] { initialPointWire1[0] + vector1[0], initialPointWire1[1] + vector1[1] };
-
-                int[] initialPointWire2 = new int[2] { 0, 0 };
-
-                for (int j = 1; j < wires[1].Length; j++)
-                {
-                    int[] vector2 = TransformToVector(wires[0][i]);
-
-                    int[] finalPointWire2 = new int[] { initialPointWire2[0] + vector2[0], initialPointWire2[1] + vector2[1] };
-                    
-                    //if()
-                    //{
-                    
-                    //}
-                    
-                    initialPointWire2 = finalPointWire2;
-                }
-
-                initialPointWire1 = finalPointWire1;
             }
+
+            return minDistance;
+        }
+
+        private List<int[]> GetCrossings(List<int[]> pointsWire1, List<int[]> pointsWire2)
+        {
+            List<int[]> crossings = new List<int[]>();
+            try
+            {
+                for (int i = 0; i < pointsWire1.Count; i++)
+                {
+                    for (int j = 0; j < pointsWire2.Count; j++)
+                    {
+                        if (pointsWire1[i][0] == pointsWire2[j][0] && pointsWire1[i][1] == pointsWire2[j][1])
+                            crossings.Add(pointsWire1[i]);
+                    }
+                }
+            }
+            catch { }
             return crossings;
         }
 
-        private void MinManhattanDistance()
+        private void PrintManhattanDistance(int distance)
         {
-            int distance = 0;
-
-            if(distance > 0 )
+            if (distance > 0)
                 Console.WriteLine("Manhattan distance from the central port to the closest intersection is {0}", distance);
             else
                 Console.WriteLine("No intersections");
         }
 
-        private int[] TransformToVector(string path)
+        private List<int[]> TransformToArrayOfPoints(string[] wire)
         {
-            int[] point = new int[2] { 0, 0 };
-            string direction = path.Substring(0,1);
-            int magnitude = 0;
-            int.TryParse(path.Substring(1), out magnitude);
-            switch (direction)
-            {
-                case "U":
-                    point[1] = magnitude;
-                    break;
-                case "D":
-                    point[1] = (-1) * magnitude;
-                    break;
-                case "L":
-                    point[0] = (-1) * magnitude;
-                    break;
-                case "R":
-                    point[0] = magnitude;
-                    break;
+            List<int[]> ListPoints = new List<int[]>();
+            int[] pointInitial = new int[2] { 0, 0 };
+
+            for (int i = 0; i < wire.Length; i++)
+            {                
+                string direction = wire[i].Substring(0, 1);
+                int magnitude = 0;
+                int.TryParse(wire[i].Substring(1), out magnitude);
+                for (int j = 0; j < magnitude; j++)
+                {
+                    int[] pointFinal = new int[2] { 0, 0 };
+                    int[] a = DIRECTIONS[direction];
+                    pointFinal[0] = pointInitial[0] + a[0];
+                    pointFinal[1] = pointInitial[1] + a[1];
+
+                    pointInitial[0] = pointFinal[0];
+                    pointInitial[1] = pointFinal[1];
+                    ListPoints.Add(pointFinal);
+                }                
             }
 
-            return point;
+            return ListPoints;
         }
     }
 }
-
-
-//https://math.stackexchange.com/questions/139600/how-do-i-calculate-euclidean-and-manhattan-distance-by-hand
-
-
-
